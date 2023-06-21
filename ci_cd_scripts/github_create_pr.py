86d5491c3d7b13c7a2029b651364391b2cf9ba25
Randy Suarez Rodes <rsrjohnson@gmail.com>
2023-06-21 14:20:15 -0500

    testing pr creation

--- ci_cd_scripts/github_create_pr.py
+++ ci_cd_scripts/github_create_pr.py
@@ -14,19 +14,19 @@ parser.add_argument('--destination', type= str)
 
 args = parser.parse_args()
 
-_REPO_OWNER = args.repo_owner
-_REPO_SLUG = args.repo_slug
-_AUTH_TOKEN = args.auth_token
+REPO_OWNER = args.repo_owner
+REPO_SLUG = args.repo_slug
+AUTH_TOKEN = args.auth_token
 SOURCE_BRANCH = args.source
 DESTINATION_BRANCH = args.destination
 
 
-url = f"https://api.github.com/repos/{_REPO_OWNER}/{_REPO_SLUG}/pulls"
+url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_SLUG}/pulls"
 
 
 headers = {
     "Accept": "application/vnd.github+json",
-    "Authorization": f"Bearer {_AUTH_TOKEN}"
+    "Authorization": f"Bearer {AUTH_TOKEN}"
     }
 
 
@@ -38,4 +38,10 @@ payload = json.dumps(
     }
 )
 
+response = requests.request(
+    "POST",
+    url,
+    data=payload,
+    headers=headers
+    )
 
