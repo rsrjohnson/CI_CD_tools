06b07bcb9ee6ed88f3d268e5348944d83818ae78
Randy Suarez Rodes <30844367+rsrjohnson@users.noreply.github.com>
2024-06-04 11:39:34 -0500

    debug better script

--- ci_cd_scripts/github_create_pr.py
+++ ci_cd_scripts/github_create_pr.py
@@ -23,63 +23,71 @@ print(SOURCE_BRANCH)
 print(DESTINATION_BRANCH)
 
 
-url=f"https://api.github.com/repos/{REPO_SLUG}/pulls"
+def createPullRequest(url):
 
-print(url)
+    params = {
+        "title": f"Pull request from {SOURCE_BRANCH} to {DESTINATION_BRANCH}",
+        "head": SOURCE_BRANCH,
+        "base": DESTINATION_BRANCH
+    }
+
+    response = requests.request(
+        "POST",
+        url=url,
+        headers=headers,
+        data=json.dumps(params)
+    )
+
+
+    if response.status_code==201:
+        #print("PR created successfully.")
+
+        pr_number = response.json()["number"]
+
+        print(pr_number)
+
+        # print(json.dumps(response.json(),indent=4))
+        # print("source:",SOURCE_BRANCH)
+        # print("target:",TARGET_BRANCH)
+
+    else:
+        raise(Exception(response.text))
+
+
+api_url = f"https://api.github.com/repos/{REPO_SLUG}/"
+
+#api_action = "commits"
+
+api_action = "pulls"
+
+query = f"?q=is:pr+state:open+head:{SOURCE_BRANCH}+base:{DESTINATION_BRANCH}"
+
+url = api_url + api_action + query
 
 headers = {
     "Accept": "application/vnd.github+json",
     "Authorization": f"token {AUTH_TOKEN}"
     }
 
-
-
-# Check if the pull request already exists with the same source and target branches
-params={
-        'head': f"rsrjohnson:{SOURCE_BRANCH}",
-        'base': DESTINATION_BRANCH
-    }
-
-
-
-response = requests.request(
-    "POST",
-    url,
-    data=params,
+response_get = requests.request(
+    "GET",
+    url=url,
     headers=headers
-    )
-
-print(response)
-
-# If a pull request already exists, print a message and exit
-if response.ok:
-    print("I am here")
-    pull_requests = response.json()
-    for pull_request in pull_requests:
-        #if pull_request['head']['ref'] == SOURCE_BRANCH and pull_request['base']['ref'] == DESTINATION_BRANCH:
-            #print('Pull request already exists with the same source and target branches.')
-        pull_request_data = response.json()["number"]
-        print(json.dumps(pull_request_data))
-    exit()        
-        #else:
-
-payload = json.dumps(
-    {
-        "title": f"Auto PR from {SOURCE_BRANCH} to {DESTINATION_BRANCH}",
-        "head": SOURCE_BRANCH,
-        "base": DESTINATION_BRANCH
-    }
 )
 
-response = requests.request(
-    "POST",
-    url,
-    data=payload,
-    headers=headers
-    )
 
-if response.status_code == 201:
-    pull_request_number = response.json()["number"]
-    print(pull_request_number)
+if response_get.status_code==200:
+    jsonvar = response_get.json()
+    if len(jsonvar)==0:
+        createPullRequest(api_url+api_action)
+
+    else:   
+        pr_number = str(response_get.json()[0]["number"])
+        #print(f"PR from {SOURCE_BRANCH} to {TARGET_BRANCH} already exists with #{pr_number}.")
+        print(pr_number)
+    # env_file = os.getenv('GITHUB_ENV') # Get the path of the runner file
+    # # write to the file
+    # with open(env_file, "a") as env_file:
+    #     env_file.write(f"PR_NUMBER={str(pr_number)}")
 else:
-    print(f'Failed to create pull request: {response.text}')
+    raise(Exception(response_get.text))
